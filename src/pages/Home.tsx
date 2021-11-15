a895e4351b9e69fea580c105547efa07b300d132
Thang Le Dinh <thangld2407@gmail.com>
2021-11-16 04:38:36 +0700

    update sound

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,59 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { play, pause, radioOutline } from 'ionicons/icons'
 import './Home.css';
+import ReactAudioPlayer from 'react-audio-player';
 
 const Home: React.FC = () => {
+  let musicPlay: ReactAudioPlayer | null;
+  let bell : ReactAudioPlayer | null;
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Application Ring Bell</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+      <IonContent>
+        <IonItem>
+          <ReactAudioPlayer
+            ref={(element) => { musicPlay = element; }}
+            src="./assets/sound/lcslg.mp3"
+          />
+          <IonLabel><strong>Playing Music </strong></IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonButton onClick={() => musicPlay?.audioEl.current?.play()}>
+            <IonIcon slot="icon-only" icon={play}></IonIcon>
+          </IonButton>
+          <IonButton onClick={() => musicPlay?.audioEl.current?.pause()}>
+            <IonIcon slot="icon-only" icon={pause} ></IonIcon>
+          </IonButton>
+        </IonItem>
+        <IonItem className="react-audio">
+        <ReactAudioPlayer
+          ref={(element) => { bell = element; }}
+          src="./assets/sound/Sound.mp3"
+        />
+          <IonLabel>Ring Bell Button</IonLabel>
+        </IonItem>
+        <IonItem>
+        <IonButton onClick={() =>bell?.audioEl.current?.play() }>Ring a Bell</IonButton>
+
+        </IonItem>
+        <IonItem>
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <strong>Vibrate Control Device</strong>
           </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        </IonItem>
+        <IonItem>
+          <IonLabel>
+            <IonToolbar>
+              <IonButton onClick={() => navigator.vibrate(2000)}>
+                <strong>Click here to Vibrate Device </strong> <br /><IonIcon slot="icon-only" icon={radioOutline}></IonIcon>
+              </IonButton>
+            </IonToolbar>
+          </IonLabel>
+        </IonItem>
       </IonContent>
     </IonPage>
   );
